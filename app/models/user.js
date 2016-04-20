cd730be476f89b0910d4b8a6d3c30d82ba104491
snewcomer <snewcomer24@gmail.com>
2016-04-20 11:39:47 -0700

    Showing how DI does not work w/o explicitly passing simple store to object that needs the DI

--- app/models/user.js
+++ app/models/user.js
@@ -1,7 +1,12 @@
+import Ember from 'ember';
 import { attr, Model } from 'ember-cli-simple-store/model';
 
 var User = Model.extend({
+    simpleStore: Ember.inject.service(),
     name: attr(),
+    wat: Ember.computed(function() {
+      return this.get('simpleStore').find('user');
+    }),
     serialize: function() {
         return {
             name: this.get('name')
--- tests/unit/user-deserializer-test.js
+++ tests/unit/user-deserializer-test.js
@@ -1,6 +1,7 @@
 import {module, test} from 'admin/tests/helpers/qunit';
 import users from 'admin/vendor/fixtures/users';
 import UserDeserializer from 'admin/deserializers/user';
+import User from 'admin/models/user';
 import registration from 'admin/tests/helpers/registration';
 
 var store;
@@ -17,4 +18,19 @@ test('api response deserialized into user models', (assert) => {
     var models = store.find('user');
     assert.equal(models.get('length'), 3);
     assert.equal(models.objectAt(0).get('name'), users.list()[0].name);
+});
+
+test('scott wat', (assert) => {
+    const user = store.push('user', {id: 1})
+    var models = store.find('user');
+    assert.equal(models.get('length'), 1);
+    assert.equal(user.get('wat').get('length'), 1);
+});
+
+test('scott wat2', (assert) => {
+    const user = User.create({simpleStore: store, id: 1})
+    store.push('user', {id: 1});
+    var models = store.find('user');
+    assert.equal(models.get('length'), 1);
+    assert.equal(user.get('wat').get('length'), 1);
 });
